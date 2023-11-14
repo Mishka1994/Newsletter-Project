917b41b76e885c58bdca742c44009787739f94a7
Mihail Samofalov <misasamofalov611@gmail.com>
2023-11-15 02:05:57 +0600

    course 6 pt.1

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('newsletter.urls'), name='newsletter')
+    path('', include('newsletter.urls', namespace='newsletter'))
 ]
